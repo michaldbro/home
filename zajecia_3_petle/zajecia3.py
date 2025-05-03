d98176a8ff9d10bb5f261da55571fa358fa610b2
michaldbro <michal.dbro@gmail.com>
2025-05-03 16:14:58 +0200

    update pliku zajecia3.py

--- zajecia_3_petle/zajecia3.py
+++ zajecia_3_petle/zajecia3.py
@@ -1,18 +1,21 @@
 print("Program Paczki")
 ile_paczek = int(input("Podaj ilość paczek: "))
-ile_kg_all = 0
-ktora_paczka = 1
-while ile_paczek > 0:
-    ile_kg = int(input(f'Podaj wage paczki {ktora_paczka}: '))
-    ile_kg_all = ile_kg_all+ile_kg
-    ile_paczek -= 1
-    ktora_paczka += 1
-print(f'paczki ważą w sumie: {ile_kg_all}')
-
-if ile_kg_all//20 <1:
-    print('Ilość paczek jaka zostanie wysłana: 1')
+if ile_paczek<1:
+    print('Brak Paczek!')
 else:
-    if ile_kg_all % 20 >0:
-        print(f'iloś paczek jaka zostanie wysłana: {(ile_kg_all//20)+1}')
-ile_paczek_wyslano = ((ile_kg_all//20)+1) * 20
-print(f'Ilość pustych KG w ostatniej paczce: {ile_paczek_wyslano-ile_kg_all}')
+    ile_kg_all = 0
+    ktora_paczka = 1
+    while ile_paczek > 0:
+        ile_kg = int(input(f'Podaj wage paczki {ktora_paczka}: '))
+        ile_kg_all = ile_kg_all+ile_kg
+        ile_paczek -= 1
+        ktora_paczka += 1
+    print(f'paczki ważą w sumie: {ile_kg_all}')
+
+    if ile_kg_all//20 <1:
+        print('Ilość paczek jaka zostanie wysłana: 1')
+    else:
+        if ile_kg_all % 20 >0:
+            print(f'iloś paczek jaka zostanie wysłana: {(ile_kg_all//20)+1}')
+    ile_paczek_wyslano = ((ile_kg_all//20)+1) * 20
+    print(f'Ilość pustych KG w ostatniej paczce: {ile_paczek_wyslano-ile_kg_all}')
