773e0dc34caf199c4beebcf9865e56077f4c084b
michaldbro <michal.dbro@gmail.com>
2025-05-14 15:52:55 +0200

    zajecia 3 update zadania

--- zajecia_3_petle/zajecia3.py
+++ zajecia_3_petle/zajecia3.py
@@ -7,10 +7,13 @@ else:
     ktora_paczka = 1
     while ile_paczek > 0:
         ile_kg = int(input(f'Podaj wage paczki {ktora_paczka}: '))
-        ile_kg_all = ile_kg_all+ile_kg
-        ile_paczek -= 1
-        ktora_paczka += 1
-    print(f'paczki ważą w sumie: {ile_kg_all}')
+        if ile_kg<1 or ile_kg>10:
+            break
+        else:
+            ile_kg_all = ile_kg_all+ile_kg
+            ile_paczek -= 1
+            ktora_paczka += 1
+    print("Podsumowanie:")
 
     if ile_kg_all//20 <1:
         print('Ilość paczek jaka zostanie wysłana: 1')
@@ -18,4 +21,5 @@ else:
         if ile_kg_all % 20 >0:
             print(f'iloś paczek jaka zostanie wysłana: {(ile_kg_all//20)+1}')
     ile_paczek_wyslano = ((ile_kg_all//20)+1) * 20
-    print(f'Ilość pustych KG w ostatniej paczce: {ile_paczek_wyslano-ile_kg_all}')
+    print(f'Wysłano (paczki ważą w sumie): {ile_kg_all}kg')
+    print(f'Ilość pustych kg w jednej paczce: {ile_paczek_wyslano-ile_kg_all}')
