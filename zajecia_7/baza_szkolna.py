cadf4772b56dcba14dfb4a838f2d0e50c0641a43
michaldbro <michal.dbro@gmail.com>
2025-05-23 10:42:04 +0200

    gotowa wersja 1 programu baza_szkoly.py

--- zajecia_7/baza_szkolna.py
+++ zajecia_7/baza_szkolna.py
@@ -28,35 +28,75 @@ szkola = {
     "uczniowie": [
         uczen("Adam Nowak", "1A"),
         uczen("Piotr Adamek", "3B"),
-        uczen("Marcin Kwiatek", "1A")
+        uczen("Marcin Kwiatek", "1A"),
         uczen("Aleksander Bok", "2C")
     ],
     "wychowawca": [
-        wychowawca("Piotr Kowalski", "1A"),
+        wychowawca("P", "1A"),
         wychowawca("Zygmunt Nowak", "3B"),
         wychowawca("Filip Światek", "2C")
     ],
     "nauczyciel": [
-        nauczyciel("Piotr Kowalski", "polski", ["1A", "3C"]),
+        nauczyciel("Piotr Kowalski", "polski", ["1A", "2C"]),
         nauczyciel("Zygmunt Nowak", "matematyka", ["3B", "6E"]),
-        nauczyciel("Filip Światek", "bilogia", ["2C", "8C"])
+        nauczyciel("Filip Światek", "bilogia", ["2C", "8C"]),
+        nauczyciel("Anatol Kobza", "geografia", ["1A", "8C"])
     ]
 }
 
 def wyszukaj_ucznia(wyb_klasy):
     uczniowie1 =[]
-    for klasa_szukaj in szkola.get("uczniowie"):
+    for klasa_szukaj in szkola.get("uczniowie",[]):
         if klasa_szukaj.jaka_klasa == wyb_klasy:
-            uczniowie1.append(klasa_szukaj)
+            uczniowie1.append(klasa_szukaj.imie_nazwisko)
     return uczniowie1
+    if uczniowie1:
+        return uczniowie1[0]
+    else:
+        return None
+
+
+def wyszukaj_wychowawce_3(wyb_klasy3): #uzycie w case4
+    wychowawca3 =[]
+    wychowawca33 = None
+    for klasa_szukaj in szkola.get("wychowawca"):
+        if klasa_szukaj.imie_nazwisko == wyb_klasy3:
+            wychowawca3.append(klasa_szukaj.jaka_klasa)
+            wychowawca33 = wychowawca3[0]
+    return wychowawca33
 
 def wyszukaj_wychowawce(wyb_klasy1):
     wychowawca1 =[]
     for klasa_szukaj in szkola.get("wychowawca"):
         if klasa_szukaj.jaka_klasa == wyb_klasy1:
-            wychowawca1.append(klasa_szukaj)
+            wychowawca1.append(klasa_szukaj.imie_nazwisko)
     return wychowawca1
 
+def wyszukaj_lekcje_n(wyb_klasy2):
+    nauczycie = []
+    nauczycie1 = []
+    for klasa_szukaj in szkola.get("nauczyciel"):
+        if wyb_klasy2 in klasa_szukaj.ktora_klasa:
+            nauczycie.append(klasa_szukaj.jaki_przedmiot)
+            nauczycie1.append(klasa_szukaj.imie_nazwisko)
+    return nauczycie, nauczycie1
+
+def wyszukaj_imie_ucznia(wyb_imie):
+    uczniowie2 = []
+    for imie_szukaj in szkola.get("uczniowie", []):  # Zabezpieczenie przed barkiem danych, podstawi pusta liste
+        if imie_szukaj.imie_nazwisko == wyb_imie:
+            uczniowie2.append(imie_szukaj.jaka_klasa)
+    if uczniowie2:  # Sprawdzenie, czy nie puste
+        return uczniowie2[0]
+    else:
+        return None
+
+def wyszukaj_nauczyciela(imie):
+    nauczycie = []
+    for klasa_szukaj in szkola.get("nauczyciel"):
+        if klasa_szukaj.imie_nazwisko == imie:
+            nauczycie.append(klasa_szukaj.ktora_klasa)
+    return nauczycie
 
 #-----------------------------------------------------------------------------
 while True:
@@ -103,11 +143,42 @@ while True:
                "4. wychowawca\n"
             )
             match zarzadzanie:
-                case "1":
+                case "1": #klasa
                     wyb_klasy = input('Podaj numer klasy: ')
                     wyszukaj_u = wyszukaj_ucznia(wyb_klasy)
                     wyszukaj_w = wyszukaj_wychowawce(wyb_klasy)
                     print(f"Uczniowie: {wyszukaj_u}")
                     print(f"Wychowawca: {wyszukaj_w}")
+
+                case "2": # uczen
+                    wyb_imie = input('Podaj imię i nazwisko ucznia: ')
+                    wyszukaj_i = wyszukaj_imie_ucznia(wyb_imie)
+                    if wyszukaj_i:
+                        print(f"Nr klasy: {wyszukaj_i}")
+                        wyszukaj_l, wyszukaj_l2 = wyszukaj_lekcje_n(wyszukaj_i)
+                        if wyszukaj_l and wyszukaj_l2:
+                            print("Przedmioty:", wyszukaj_l)
+                            print("Nauczyciele:", wyszukaj_l2)
+                        else:
+                            print("Nie znaleziono żadnych lekcji dla podanej klasy.")
+                    else:
+                        print("Imię ucznia nie pasuje do danych w bazie.")
+
+                case "3":  # nauczyciel
+                    imie_n = input('Podaj imie i nazwisko nauczyciela: ')
+                    wyszukaj_n = wyszukaj_nauczyciela(imie_n)
+                    print(f"Prowadzone Klasy: {wyszukaj_n}")
+                case "4": #wychowawca
+                    wyb_imie = input('Podaj imię i nazwisko Wychowawcy: ')
+                    wyszukaj_i = wyszukaj_wychowawce_3(wyb_imie)
+                    if wyszukaj_i:
+                        print(f"Nr klasy: {wyszukaj_i}")
+                        wyszukaj_l2 = wyszukaj_ucznia(wyszukaj_i)
+                        if wyszukaj_l2:
+                            print("Uczniowie:", wyszukaj_l2)
+                        else:
+                            print("Nie znaleziono.")
+                    else:
+                        print("Imię Wychowawcy nie pasuje do danych w bazie.")
         case "3":
             break
