adf40292b2fdae878eab6c2d6f7db83cd2e53985
michaldbro <michal.dbro@gmail.com>
2025-06-05 15:30:48 +0200

    dodany gotowy program z zajec 11

--- zajecia_11/file_handler.py
+++ zajecia_11/file_handler.py
@@ -6,6 +6,7 @@ class FileHandler:
         self.output_file = output_file1
         self.ldata = self.load()
         self.transform = transform1
+        self.new = self.do_transform()
 
     def load(self):
         with open(self.input_file) as file:
@@ -21,14 +22,12 @@ class FileHandler:
     def save(self):
         with open(self.output_file, 'w+', newline='') as file:
             writer = csv.writer(file)
-            for row in self.ldata:
+            for row in self.new: #self.ldata:
                 writer.writerow(row)
 
-'''
     def do_transform(self):
-        lista =[]
-        for transformation in self.transform:
-            transformation_list = transformation.split(" ")
-            operation = lista.append(transformation_list)
-        return print('transformacja'), print(operation), print('koniec transformacji')
-'''
+        lista_list = []
+        for czesc_listy in self.transform:
+            lista_list.append(czesc_listy.split(','))
+        #lista_list = [czesc_listy.split(',') for czesc_listy in self.transform]
+        return lista_list
